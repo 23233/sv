a07996d7c1ec42fd9fdd816ab83b9aa72bab8fe6
23233 <1@qq.com>
2020-07-06 19:39:34 +0800

    new version delete more log file

--- log.go
+++ log.go
@@ -1,24 +1,14 @@
 package sv
 
 import (
-	"io"
 	"log"
 	"os"
 )
 
 var (
-	Info    *log.Logger
 	Warning *log.Logger
-	Error   *log.Logger
 )
 
 func init() {
-	errFile, err := os.OpenFile("sv.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalln("open log file fail：", err)
-	}
-	Info = log.New(os.Stdout, "[sv] Info:", log.Ldate|log.Ltime|log.Lshortfile)
 	Warning = log.New(os.Stdout, "[sv] Warning:", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(io.MultiWriter(os.Stderr, errFile), "[sv] Error:", log.Ldate|log.Ltime|log.Lshortfile)
-
 }
